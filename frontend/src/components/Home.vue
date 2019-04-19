e8e7af5efaf9f7c621123dd40617ef9ab2956e2c
Anca <misinanca@gmail.com>
2019-04-19 21:15:09 +0300

    Add styling and remove tests

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="home">
-    This is a template homepage page
+  <div class="masthead">
+    <div class="container d-flex h-100 align-items-center">
+      <div class="mx-auto text-center">
+        <h1 class="mx-auto my-0 text-uppercase">Bill tracker</h1>
+        <h2 class="text-white-50 mx-auto mt-2 mb-5">
+          A free way of managing all your money. Our Bill Tracking tool lets you enter all your monthly bills,
+          what amount you owe, when the bill is due and see spending graphics.</h2>
+        <router-link to="register" class="btn btn-primary js-scroll-trigger">Get Started</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,21 +21,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
