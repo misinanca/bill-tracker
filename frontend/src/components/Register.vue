948c32a9b05b53ac539ae4aae5aff4f98eb2161d
Anca <misinanca@gmail.com>
2019-05-05 15:43:55 +0300

    Add validation

--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -14,63 +14,81 @@
       <div class="form-row">
         <div class="form-group col-md-6">
           <input
+            v-validate="'required|min:6'"
             v-model="user.username"
             type="text"
             class="form-control"
             id="inputUsername"
+            name="username"
             placeholder="Username"
           />
+          <span class="error-message">{{ errors.first('username') }}</span>
         </div>
         <div class="form-group col-md-6">
           <input
+            v-validate="'required|min:6'"          
             v-model="user.password"
             type="password"
             class="form-control"
             id="inputPass"
+            name="password"
             placeholder="Password"
           />
+          <span class="error-message">{{ errors.first('password') }}</span>          
         </div>
       </div>
       <div class="form-row">
         <div class="form-group col-md-6">
           <input
+            v-validate="'required|alpha'"
             v-model="user.firstName"
             type="text"
             class="form-control"
             id="inputFirstName"
+            name="firstName"
             placeholder="First Name"
           />
+          <span class="error-message">{{ errors.first('firstName') }}</span>
         </div>
         <div class="form-group col-md-6">
           <input
+            v-validate="'required|alpha'"
             v-model="user.lastName"
             type="text"
             class="form-control"
             id="inputLastName"
+            name="lastName"
             placeholder="Last Name"
           />
+          <span class="error-message">{{ errors.first('lastName') }}</span>
         </div>
       </div>
       <div class="form-row">
         <div class="form-group col-md-12">
           <input
+            v-validate="'required'"
             v-model="user.address"
             type="text"
             class="form-control"
             id="inputAddress"
+            name="address"
             placeholder="Address"
           />
+          <span class="error-message">{{ errors.first('address') }}</span>
         </div>
       </div>
       <div class="form-row">
         <div class="form-group col-md-12">
           <input
+            v-validate="'required|email'"
             v-model="user.email"
             type="email"
             class="form-control"
             id="inputEmail"
+            name="email"
             placeholder="Email address"
           />
+          <span class="error-message">{{ errors.first('email') }}</span>
         </div>
       </div>
       <button class="btn btn-primary mx-auto">Register</button>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,11 +4,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import BootstrapVue from 'bootstrap-vue';
+import VeeValidate from 'vee-validate';
 
 Vue.config.productionTip = false
 
 // Bootstrap
 Vue.use(BootstrapVue)
+Vue.use(VeeValidate);
 
 new Vue({
     router,
